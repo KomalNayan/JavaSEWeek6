d4dd8b277739aa2a649e38885bf07939dfeb5871
Komal Nayan <31290547+KomalNayan@users.noreply.github.com>
2021-02-27 20:40:45 +0530

    Added Gender enum

---
+++ src/com/komalnayangalla_c0779428_week6/Gender.java
@@ -0,0 +1,8 @@
+package com.komalnayangalla_c0779428_week6;
+
+public enum Gender
+{
+        MALE,
+        FEMALE,
+        OTHER
+}
---
+++ src/com/komalnayangalla_c0779428_week6/Main.java
@@ -0,0 +1,8 @@
+package com.komalnayangalla_c0779428_week6;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
